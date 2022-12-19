6eaa38646d22a4c53b08bfd95583f595d780b712
afoda111 <afoda1327@gmail.com>
2022-12-19 05:51:36 +0200

    first build

---
+++ helper.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const accountSid = process.env.SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+const messagesArray = [
+  "I'm here to help you Buddy",
+  "I got you Buddy",
+  "You are way stronger than what you think you are",
+  "You will get through this just hold on tight",
+  "They don't know me son !!",
+  "Look at you and see within, see how powerfull you'are",
+];
+
+const returnDifferentMessage = (Array) => {
+  return Array[Math.floor(Math.random() * messagesArray.length)];
+};
+
+const sendMessage = async (senderId) => {
+  await client.messages
+    .create({
+      body: `${returnDifferentMessage(messagesArray)}`,
+      from: "whatsapp:+14155238886",
+      to: senderId,
+    })
+    .then((message) => console.log(message.sid))
+    .done();
+};
+
+module.exports = {
+  sendMessage,
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const sendMessage = require("./helper");
+const App = express();
+App.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+App.use(bodyParser.json());
+const port = process.env.PORT;
+App.listen(port, () => {
+  console.log("service Running");
+});
+
+App.get("/", (req, res) => {
+  res.send("hello");
+});
+
+App.post("/whatsapp", async (req, res) => {
+  const senderId = req.body.From;
+
+  await sendMessage.sendMessage(senderId);
+});
